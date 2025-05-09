8209ef03b8524bd9625042e801bbc71ebb7c410e
thien <thien@DESKTOP-INQH0L7>
2025-05-09 21:49:22 +0700

    expense_tracker_app

--- lib/features/onboarding/presentation/widgets/onboarding_page_view.dart
+++ lib/features/onboarding/presentation/widgets/onboarding_page_view.dart
@@ -1,9 +1,7 @@
 import 'package:expense_tracker_app/core/assets/app_images.dart';
-import 'package:expense_tracker_app/core/assets/app_vectors.dart';
 import 'package:expense_tracker_app/core/languages/app_localizations.dart';
 import 'package:expense_tracker_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class OnboardingData {
   final String title;
@@ -22,26 +20,27 @@ class OnboardingPageView extends StatelessWidget {
 
   final PageController pageController;
 
+
+  static List<OnboardingData> data(BuildContext context) => [
+    OnboardingData(
+      title: AppLocalizations.of(context)!.onboardingTitle1,
+      subTitle: AppLocalizations.of(context)!.onboardingSubTitle1,
+      image: AppImages.onboardingImage1,
+    ),
+    OnboardingData(
+      title: AppLocalizations.of(context)!.onboardingTitle2,
+      subTitle: AppLocalizations.of(context)!.onboardingSubTitle2,
+      image: AppImages.onboardingImage2,
+    ),
+    OnboardingData(
+      title: AppLocalizations.of(context)!.onboardingTitle3,
+      subTitle: AppLocalizations.of(context)!.onboardingSubTitle3,
+      image: AppImages.onboardingImage3,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    List<OnboardingData> data = [
-      OnboardingData(
-        title: AppLocalizations.of(context)!.onboardingTitle1,
-        subTitle: AppLocalizations.of(context)!.onboardingSubTitle1,
-        image: AppImages.onboardingImage1,
-      ),
-      OnboardingData(
-        title: AppLocalizations.of(context)!.onboardingTitle2,
-        subTitle: AppLocalizations.of(context)!.onboardingSubTitle2,
-        image: AppImages.onboardingImage2,
-      ),
-      OnboardingData(
-        title: AppLocalizations.of(context)!.onboardingTitle3,
-        subTitle: AppLocalizations.of(context)!.onboardingSubTitle3,
-        image: AppImages.onboardingImage3,
-      ),
-    ];
-
     return PageView.builder(
       controller: pageController,
       itemCount: 3,
@@ -52,7 +51,7 @@ class OnboardingPageView extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
               child: Image.asset(
-                data[index].image,
+                data(context)[index].image,
                 width: 312,
                 // height: MediaQuery.of(context).size.height < 705
                 //     ? 312 - (705 - MediaQuery.of(context).size.height)
@@ -64,7 +63,7 @@ class OnboardingPageView extends StatelessWidget {
               child: Column(
                 children: [
                   Text(
-                    data[index].title,
+                    data(context)[index].title,
                     style: TextStyle(
                       fontFamily: 'Inter',
                       fontWeight: FontWeight.w700,
@@ -78,7 +77,7 @@ class OnboardingPageView extends StatelessWidget {
                     height: 16,
                   ),
                   Text(
-                    data[index].subTitle,
+                    data(context)[index].subTitle,
                     style: const TextStyle(
                       fontFamily: 'Inter',
                       fontSize: 16,
